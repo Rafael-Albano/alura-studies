c13a3c084f4e6ad3f60b262c998b35f9aeeef7e4
Rafael-Albano <rafael.albano88@gmail.com>
2022-10-08 14:08:19 -0300

    feat: Button components

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,12 @@
+import { Component, ReactNode } from "react";
+
+// CLASS COMPONENTS.
+class Button extends Component {
+    render(): ReactNode {
+        return(
+            <button>Save</button>
+        )
+    }
+}
+
+export { Button }
